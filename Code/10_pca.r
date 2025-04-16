a925b428080d1401620f9526dca5564cc4cd4c40
marialuceloche <marialuceloche@libero.it>
2025-04-16 14:10:53 +0200

    Update and rename 09_pca.r to 10_pca.r

--- Code/10_pca.r
+++ Code/10_pca.r
@@ -9,6 +9,10 @@ im.list()
 sent = im.import("sentinel.png")
 sent = c(sent[[1]], sent[[2]], sent[[3]])
 
+# NIR = band 1
+# red = band 2
+# green = band 3
+
 # fasi della PCA:
 # 1. campionamento
 sample = spatSample(sent, 100)
@@ -31,3 +35,11 @@ varnir = focal(sent[[1]], matrix(1/9, 3, 3), fun = sd)
 d1 = im.ggplot(varpca)
 d2 = im.ggplot(varnir)
 d1 + d2
+
+
+# altro metodo:
+sentpca = im.pca(sent, n_samples=100000)
+tot = 72 + 59 + 6
+72 * 100 / tot
+sdpc1 = focal(sentpca[[1]], w=c(3,3), fun="sd")
+plot(sdpc1)   
